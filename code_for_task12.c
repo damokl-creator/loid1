61619a1f57aedbf96df48da9886fc144f910ed3b
Вихров Дмитрий Денисович ТКИ-141 <amidamid228@gmail.com>
2025-10-22 23:12:58 +0300

    Add files via upload

---
+++ code_for_task12.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * @brief переводит вес из грамм в унции
+ * @param l вес в граммах
+ * @return возвращает рассчитаный вес в унциях
+ */
+double get_weight_in_ounces (const double l);
+
+/**
+ * @brief точка входа в программу
+ * @return возвращает 0, если код выполнен корректно
+ */
+int main(void)
+{
+    double x = 0;
+    scanf("%lf", &x);
+    printf("%.2lf", get_weight_in_ounces(x));
+
+    return 0;
+}
+
+double get_weight_in_ounces (const double l)
+{
+    return l / 28.3;
+}
