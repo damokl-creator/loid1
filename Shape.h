52fd3c626850922fb9e0be1174fc2af29cd940aa
Вихров Дмитрий Денисович ТКИ-141 <amidamid228@gmail.com>
2026-05-18 02:03:35 +0300

    Create Shape.h

---
+++ Shape.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class Shape
+{
+public:
+    virtual std::string toString() const = 0;
+    virtual double getArea() const = 0;
+    virtual double getPerimeter() const = 0;
+    virtual double getRadius() const = 0;
+    virtual void Read(std::istream& is) = 0;
+    virtual ~Shape() = default;
+
+    friend std::ostream& operator<<(std::ostream& os, const Shape& shape)
+    {
+        os << shape.toString();
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Shape& shape)
+    {
+        shape.Read(is);
+        return is;
+    }
+};
