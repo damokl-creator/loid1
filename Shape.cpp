6e532c39d160dd5ddb37c04fe640b27a29477602
Вихров Дмитрий Денисович ТКИ-141 <amidamid228@gmail.com>
2026-05-23 16:17:53 +0300

    Create Shape.cpp

---
+++ Shape.cpp
@@ -0,0 +1,13 @@
+#include "Shape.h"
+
+std::ostream& operator<<(std::ostream& os, const Shape& shape)
+{
+    os << shape.toString();
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Shape& shape)
+{
+    shape.Read(is);
+    return is;
+}
