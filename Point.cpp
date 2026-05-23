548275d138494b3ea6d9d68d847d9b4c46993b8c
Вихров Дмитрий Денисович ТКИ-141 <amidamid228@gmail.com>
2026-05-23 16:05:41 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -5,7 +5,7 @@
 const double Point::EPS = std::numeric_limits<double>::epsilon() * 1e7;
 double Point::maxCoord = 1000.0;
 
-Point::Point(double x, double y) : x(x), y(y) 
+Point::Point(const double x, const double y) : x(x), y(y)
 {
     if (x < 0 || x > maxCoord + EPS || y < 0 || y > maxCoord + EPS)
         throw std::out_of_range("Point coordinates out of screen bounds");
