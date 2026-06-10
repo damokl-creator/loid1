2b858e55027a88feb3a054dc11df979807720fcc
Вихров Дмитрий Денисович ТКИ-141 <amidamid228@gmail.com>
2026-06-10 09:23:07 +0300

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -6,13 +6,13 @@ class Circle : public Shape
 {
 private:
     Point center;
-    double radius;
+    const double radius;
     static const double EPS;
 
 public:
     Circle();
     Circle(const Point& center, const Point& pointOnCircle);
-    Circle(double cx, double cy, double px, double py);
+    Circle(const double cx, const double cy, const double px, const double py);
 
     double getArea() const override;
     double getPerimeter() const override;
